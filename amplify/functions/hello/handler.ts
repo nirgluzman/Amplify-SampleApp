3e4c786f8cc3ea966d5ffb7e03d20b29b209fb39
Nir Gluzman <nir.gluzman@gmail.com>
2025-05-19 22:46:40 +0200

    feat: add environment variables to sayHello function

--- amplify/functions/hello/handler.ts
+++ amplify/functions/hello/handler.ts
@@ -1,5 +1,5 @@
 /**
- * Lambda Function Handler for the sayHello GraphQL Query
+ * Lambda Function Handler for the sayHello GraphQL Query.
  *
  * This handler processes requests from the GraphQL API's sayHello query, extracting the name parameter
  * and returning a personalized greeting.
@@ -7,7 +7,11 @@
 
 import type { Schema } from '../../data/resource';
 
-// define the handler type based on the GraphQL schema definition
+// import environment variables which are specific to sayHello Lambda function.
+// $amplify path is a special path alias to reference generated resources.
+import { env } from '$amplify/env/sayHello';
+
+// define the handler type based on the GraphQL schema definition.
 type handlerType = Schema['sayHello']['functionHandler'];
 
 /**
@@ -17,9 +21,12 @@ type handlerType = Schema['sayHello']['functionHandler'];
  * @returns A string greeting with the provided name
  */
 export const handler: handlerType = async (event) => {
-  // extract the name parameter from the event arguments
+  const region = env.REGION;
+  const tableName = env.TABLE_NAME;
+
+  // extract the name parameter from the event arguments.
   const { name } = event.arguments;
 
-  // return a personalized greeting
-  return `Hello, ${name}!`;
+  // return a personalized greeting.
+  return `Hello, ${name}! region: ${region} table name: ${tableName}`;
 };
--- amplify/functions/hello/resource.ts
+++ amplify/functions/hello/resource.ts
@@ -1,9 +1,15 @@
 import { defineFunction } from '@aws-amplify/backend';
 
+const environmentVariables = {
+  REGION: 'us-east-1',
+  TABLE_NAME: 'myTable',
+};
+
 export const sayHello = defineFunction({
   name: 'sayHello',
   entry: './handler.ts',
   bundling: {
     minify: false,
   },
+  environment: environmentVariables,
 });
